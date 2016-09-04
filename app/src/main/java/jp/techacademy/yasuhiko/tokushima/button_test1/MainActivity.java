be318230d7082342dc7d310cc79837eda6e9eef8
koutaro <julianseed@gmail.com>
2016-09-04 16:24:47 +0900

    ボタンをタップした時にログを出力する別の書き方に変更

--- app/src/main/java/jp/techacademy/yasuhiko/tokushima/button_test1/MainActivity.java
+++ app/src/main/java/jp/techacademy/yasuhiko/tokushima/button_test1/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +14,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(this);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("UI_PARTS", "ボタンをタップしました。");
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        Log.d("UI_PARTS", "ボタンをタップしました。");
-    }
 }
